320cdcb77db6865e1913d64f1f99012cd36dbc2b
Sukendh <sukendh.p@netobjex.com>
2021-03-04 13:46:05 +0530

    categorylayout fix

--- lib/screen/category_screen.dart
+++ lib/screen/category_screen.dart
@@ -5,6 +5,7 @@ import 'package:zaincart_app/screen/filter_screen.dart';
 import 'package:zaincart_app/utils/constants.dart';
 import 'package:zaincart_app/widgets/zc_account.dart';
 import 'package:zaincart_app/widgets/zc_appbar_title.dart';
+import 'package:zaincart_app/widgets/zc_category_item.dart';
 import 'package:zaincart_app/widgets/zc_menu.dart';
 import 'package:zaincart_app/widgets/zc_product_item.dart';
 import 'package:zaincart_app/widgets/zc_text.dart';
@@ -67,90 +68,80 @@ class _CategoryScreenState extends State<CategoryScreen> {
                     child: Row(
                       children: [
                         Expanded(
-                          child:  Padding(
-                                    padding: EdgeInsets.only(right: 10.0),
-                                    child: ListView.builder(
-                                        itemCount: homeBloc.categories
-                                            .where((e) =>
-                                                e.categoryId ==
-                                                homeBloc.selectedCategoryId)
-                                            .first
-                                            .subcategory
-                                            .length,
-                                        scrollDirection: Axis.horizontal,
-                                        itemBuilder:
-                                            (BuildContext ctxt, int index) {
-                                          return InkWell(
-                                            onTap: () {
-                                              homeBloc.selectedSubCategoryId =
-                                                  homeBloc.categories
-                                                      .where((e) =>
-                                                          e.categoryId ==
-                                                          homeBloc
-                                                              .selectedCategoryId)
-                                                      .first
-                                                      .subcategory[index]
-                                                      .categoryId;
-                                              
-                                              homeBloc.getProductsByCategory(
-                                                  context: context,
-                                                  categoryId: homeBloc.selectedSubCategoryId,
-                                                  pageNo: "1");
-                                            },
-                                            child: Container(
-                                              padding: EdgeInsets.only(
-                                                  left: 10.0,
-                                                  right: 10.0,
-                                                  top: 10.0),
-                                              child: Column(
-                                                children: [
-                                                  ZCText(
-                                                    text: homeBloc.categories
+                            child: Padding(
+                          padding: EdgeInsets.only(right: 10.0),
+                          child: ListView.builder(
+                              itemCount: homeBloc.categories
+                                  .where((e) =>
+                                      e.categoryId ==
+                                      homeBloc.selectedCategoryId)
+                                  .first
+                                  .subcategory
+                                  .length,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (BuildContext ctxt, int index) {
+                                return InkWell(
+                                  onTap: () {
+                                    homeBloc.selectedSubCategoryId = homeBloc
+                                        .categories
+                                        .where((e) =>
+                                            e.categoryId ==
+                                            homeBloc.selectedCategoryId)
+                                        .first
+                                        .subcategory[index]
+                                        .categoryId;
+
+                                    homeBloc.getProductsByCategory(
+                                        context: context,
+                                        categoryId:
+                                            homeBloc.selectedSubCategoryId,
+                                        pageNo: "1");
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.only(
+                                        left: 10.0, right: 10.0, top: 10.0),
+                                    child: Column(
+                                      children: [
+                                        ZCText(
+                                          text: homeBloc.categories
+                                              .where((e) =>
+                                                  e.categoryId ==
+                                                  homeBloc.selectedCategoryId)
+                                              .first
+                                              .subcategory[index]
+                                              .categoryName,
+                                          color: Constants.zc_font_grey,
+                                        ),
+                                        homeBloc.selectedSubCategoryId ==
+                                                homeBloc.categories
+                                                    .where((e) =>
+                                                        e.categoryId ==
+                                                        homeBloc
+                                                            .selectedCategoryId)
+                                                    .first
+                                                    .subcategory[index]
+                                                    .categoryId
+                                            ? Container(
+                                                height: 5.0,
+                                                color: Constants.zc_orange,
+                                                width: homeBloc.categories
                                                         .where((e) =>
                                                             e.categoryId ==
                                                             homeBloc
                                                                 .selectedCategoryId)
                                                         .first
                                                         .subcategory[index]
-                                                        .categoryName,
-                                                    color:
-                                                        Constants.zc_font_grey,
-                                                  ),
-                                                  homeBloc.selectedSubCategoryId ==
-                                                          homeBloc.categories
-                                                              .where((e) =>
-                                                                  e.categoryId ==
-                                                                  homeBloc
-                                                                      .selectedCategoryId)
-                                                              .first
-                                                              .subcategory[
-                                                                  index]
-                                                              .categoryId
-                                                      ? Container(
-                                                          height: 5.0,
-                                                          color: Constants
-                                                              .zc_orange,
-                                                          width: homeBloc
-                                                                  .categories
-                                                                  .where((e) =>
-                                                                      e.categoryId ==
-                                                                      homeBloc
-                                                                          .selectedCategoryId)
-                                                                  .first
-                                                                  .subcategory[
-                                                                      index]
-                                                                  .categoryName
-                                                                  .length *
-                                                              7.toDouble(),
-                                                        )
-                                                      : new Container()
-                                                ],
-                                              ),
-                                            ),
-                                          );
-                                        }),
-                                  )),
-                        
+                                                        .categoryName
+                                                        .length *
+                                                    7.toDouble(),
+                                              )
+                                            : new Container()
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }),
+                        )),
                         InkWell(
                           onTap: () {
                             Navigator.of(context).push(MaterialPageRoute(
@@ -182,14 +173,11 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   Expanded(
                     child: homeBloc.isLoading
                         ? Center(child: CircularProgressIndicator())
-                        : GridView.builder(
+                        : ListView.builder(
                             scrollDirection: Axis.vertical,
                             itemCount: homeBloc.categoryProducts.length,
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2),
                             itemBuilder: (BuildContext ctxt, int index) {
-                              return ZCProductItem(
+                              return ZCCategoryItem(
                                 product: homeBloc.categoryProducts[index],
                               );
                             }),
